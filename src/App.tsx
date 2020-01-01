10238e7c9aa6a32d39dd69a6aefbeec7415aaf92
divleo <11596691+divleo@users.noreply.github.com>
2020-01-01 21:14:06 +0200

    Add retrieve and display data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import CSS from 'csstype';
 
 import './App.css';
 
+interface ITodo {
+  id: number;
+  title: string;
+  completed: boolean;
+  important: boolean;
+}
+
+interface IProps {}
+
+interface IState {
+  todos: ITodo[];
+}
+
+class App extends React.Component<IProps, IState> {
+  state: IState = {
+    todos: [],
+  };
+
+  getHalfTodos(todos: any): any {
+    return todos.slice(0, Math.floor(todos.length / 2));
+  }
+
+  stripTodo(todo: any): ITodo {
+    return {
+      id: todo.id,
+      title: todo.title,
+      completed: todo.completed,
+      important: !todo.completed,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+      .then((response) => response.json())
+      .then((todos) => this.getHalfTodos(todos).map(this.stripTodo))
+      .then((strippedTodos) => this.setState({ todos: strippedTodos }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.todos.map((todo) => {
+          const styles: CSS.Properties = {
+            fontWeight: todo.important ? 'bold' : undefined,
+            textDecoration: todo.completed ? 'line-through' : undefined,
+          };
+
+          return (
+            <ul key={todo.id} className="todo-list">
+              <li style={styles}>
+                <span>{todo.id}</span>: {todo.title}
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
